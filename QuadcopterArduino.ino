7ef0e34a2b73b2b093be0eb51c5d3ec696fb771f
Shreyas <skraman1999@gmail.com>
2014-11-11 17:04:10 -0800

    Added libs, simplified.

--- QuadcopterArduino.ino
+++ QuadcopterArduino.ino
@@ -6,43 +6,39 @@
 #include <FIMU_ITG3200.h>
 
 #include "CommunicationUtils.h"
-#include "FreeSixIMU.h"
 #include <Wire.h>
+#include "CommunicationUtils.h"
+#include "DebugUtils.h"
+#include "PID_v1.h"
 
 float q[4];
 
 FreeSixIMU my3IMU = FreeSixIMU();
 
-double rsp, rip, rop;
-double rsn, rin, ron;
-double psp, pip, pop;
-double psn, pin, pon;
+double rollSetpoint, rollInput, rollOutput;
+double pitchSetpoint, pitchInput, pitchOutput;
 
-PID rp(&rip, &rop, &rsp, 80, 45, 0, DIRECT);
-PID rn(&rin, &ron, &rsn, 80, 45, 0, DIRECT);
-PID pp(&pip, &pop, &psp, 80, 45, 0, DIRECT);
-PID pn(&pin, &pon, &psn, 80, 45, 0, DIRECT);
+PID rollPid(&rollInput, &rollOutput, &rollSetpoint, 80, 45, 0, DIRECT);
+PID pitchPid(&pitchInput, &pitchOutput, &pitchSetpoint, 80, 45, 0, DIRECT);
 
-Servo sfr;
-Servo sfl;
-Servo sbr;
-Servo sbl;
+Servo frontRight;
+Servo frontLeft;
+Servo backRight;
+Servo backLeft;
 
-double fr = 1200;
-double fl = 1200;
-double br = 1200;
-double bl = 1200;
-
-int pfr = 1000;
-int pfl = 1000;
-int pbr = 1000;
-int pbl = 1000;
+void setup_motor() {
+  frontRight.writeMicroseconds(1000);
+  frontLeft.writeMicroseconds(1000);
+  backRight.writeMicroseconds(1000);
+  backLeft.writeMicroseconds(1000);
+  delay(15000);
+}
 
 void setup() {
-  sfr.attach(6);
-  sfl.attach(9);
-  sbr.attach(5);
-  sbl.attach(3);
+  frontRight.attach(6);
+  frontLeft.attach(9);
+  backRight.attach(5);
+  backLeft.attach(3);
 
   Serial.begin(115200);
   Wire.begin();
@@ -51,15 +47,11 @@ void setup() {
   my3IMU.init();
   delay(5);
 
-  rp.SetMode(AUTOMATIC);
-  rn.SetMode(AUTOMATIC);
-  pp.SetMode(AUTOMATIC);
-  pn.SetMode(AUTOMATIC);
+  rollPid.SetMode(AUTOMATIC);
+  pitchPid.SetMode(AUTOMATIC);
 
-  rp.SetOutputLimits(-50, 50);
-  rn.SetOutputLimits(-50, 50);
-  pp.SetOutputLimits(-50, 50);
-  pn.SetOutputLimits(-50, 50);
+  rollPid.SetOutputLimits(-50, 50);
+  pitchPid.SetOutputLimits(-50, 50);
 
   setup_motor();
 }
@@ -67,36 +59,24 @@ void setup() {
 void loop() { 
   pid();
 
-  print(1200+rop+pop, 1200+ron+pop, 1200+rop+pon, 1200+ron+pon);
+  print(1200+rollOutput+pitchOutput, 1200-rollOutput+pitchOutput, 1200+rollOutput-pitchOutput, 1200-rollOutput-pitchOutput);
 
-  sfr.writeMicroseconds(1200 + rop + pop);
-  sfl.writeMicroseconds(1200 + ron + pop);
-  sbr.writeMicroseconds(1200 + rop + pon);
-  sbl.writeMicroseconds(1200 + ron + pon);
+  frontRight.writeMicroseconds(1200 + rollOutput + pitchOutput);
+  frontLeft.writeMicroseconds(1200 - rollOutput + pitchOutput);
+  backRight.writeMicroseconds(1200 + rollOutput - pitchOutput);
+  backLeft.writeMicroseconds(1200 - rollOutput - pitchOutput);
 
   delay(60);
-}
-
-void setup_motor() {
-  sfr.writeMicroseconds(1000);
-  sfl.writeMicroseconds(1000);
-  sbr.writeMicroseconds(1000);
-  sbl.writeMicroseconds(1000);
-  delay(15000);
 }
 
 void pid() {
   my3IMU.getQ(q);
 
-  rip = q[1];
-  rin = -q[1];
-  pip = (q[2]+0.03);
-  pin = -(q[2]+0.03);
+  rollInput = q[1];
+  pitchInput = q[2];
 
-  rp.Compute();
-  rn.Compute();
-  pp.Compute();
-  pn.Compute();
+  rollPid.Compute();
+  pitchPid.Compute();
 }
 
 void print(double d1, double d2) {
