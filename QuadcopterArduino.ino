5028cd9786c2969b91e8b236cbfa0a81a7ad1958
Shreyas <skraman1999@gmail.com>
2015-02-12 23:26:35 -0800

    Wrote PID from scratch, it works!

--- QuadcopterArduino.ino
+++ QuadcopterArduino.ino
@@ -1,48 +1,33 @@
 #include <Servo.h>
-#include <PID_v1.h>
 
 #include <FreeSixIMU.h>
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
 
-#include "CommunicationUtils.h"
 #include <Wire.h>
 #include "CommunicationUtils.h"
-#include "DebugUtils.h"
-#include "PID_v1.h"
-
-typedef struct {
-  double 
-} CustomPid;
 
 float q[4];
 
 FreeSixIMU my3IMU = FreeSixIMU();
 
-double rollSetpoint, rollInput, rollOutput;
-double pitchSetpoint, pitchInput, pitchOutput;
+Servo frontRight;
+Servo frontLeft;
+Servo backRight;
+Servo backLeft;
 
-PID rollPid(&rollInput, &rollOutput, &rollSetpoint, 80, 0, 0, DIRECT);
-PID pitchPid(&pitchInput, &pitchOutput, &pitchSetpoint, 80, 0, 0, DIRECT);
+int kp = 1;
+int ki = 0;
+int kd = 0;
 
-//Servo frontRight;
-//Servo frontLeft;
-//Servo backRight;
-//Servo backLeft;
-
-void setup_motor() {
-  //frontRight.writeMicroseconds(1000);
-  //frontLeft.writeMicroseconds(1000);
-  //backRight.writeMicroseconds(1000);
-  //backLeft.writeMicroseconds(1000);
-  delay(15000);
-}
+double lastInput = 0;
+double errSum = 0;
 
 void setup() {
-  //frontRight.attach(6);
-  //frontLeft.attach(9);
-  //backRight.attach(5);
-  //backLeft.attach(3);
+  frontRight.attach(6);
+  frontLeft.attach(9);
+  backRight.attach(5);
+  backLeft.attach(3);
 
   Serial.begin(19200);
   Wire.begin();
@@ -51,21 +36,28 @@ void setup() {
   my3IMU.init();
   delay(5);
 
-  rollPid.SetMode(AUTOMATIC);
-  pitchPid.SetMode(AUTOMATIC);
+  frontRight.writeMicroseconds(1000);
+  frontLeft.writeMicroseconds(1000);
+  backRight.writeMicroseconds(1000);
+  backLeft.writeMicroseconds(1000);
+  delay(3000);
 
-  rollPid.SetOutputLimits(-50, 50);
-  pitchPid.SetOutputLimits(-50, 50);
-
-  //setup_motor();
   Serial.println("Initialized");
 }
 
-void loop() { 
-  pid();
+double lastInputRoll = 0;
+double errSumRoll = 0;
 
-//  print(1200+rollOutput+pitchOutput, 1200-rollOutput+pitchOutput, 1200+rollOutput-pitchOutput, 1200-rollOutput-pitchOutput);
-  print(rollInput, pitchInput);
+double lastInputPitch = 0;
+double errSumPitch = 0;
+
+void loop() { 
+  my3IMU.getQ(q);
+
+  double rollInput = q[1];
+  double pitchInput = q[2];
+
+  print(computePid(rollInput, 0, &errSumRoll, &lastInputRoll), computePid(pitchInput, 0, &errSumPitch, &lastInputPitch));
 
   //frontRight.writeMicroseconds(1200 + rollOutput + pitchOutput);
   //frontLeft.writeMicroseconds(1200 - rollOutput + pitchOutput);
@@ -75,14 +67,14 @@ void loop() {
   delay(60);
 }
 
-void pid() {
-  my3IMU.getQ(q);
+double computePid(double input, double setpoint, double *errSum, double *lastInput) {
+    double error = setpoint - input;
+    double dIn = *lastInput - input;
+    *errSum += error;
 
-  rollInput = q[1];
-  pitchInput = q[2];
+    *lastInput = input;
 
-  rollPid.Compute();
-  pitchPid.Compute();
+    return kp*error + ki*(*errSum) + kd*dIn;
 }
 
 void print(double d1, double d2) {
@@ -97,5 +89,3 @@ void print(double d1, double d2, double d3, double d4) {
   Serial.print(d3); Serial.print(" ");
   Serial.println(d4);
 }
-
-void pid 
